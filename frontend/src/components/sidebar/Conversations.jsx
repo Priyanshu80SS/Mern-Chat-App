999c9e905c6da07eb85c60a046d707fe69195700
Priyanshu Bhardwaj <priyanshubhardwaj099@gmail.com>
2024-04-06 18:06:22 +0530

    3rd part completed - connecting frontend and backend

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,22 @@
-import React from "react";
 import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation, index) => (
+        <Conversation
+          conversation={conversation}
+          key={conversation._id}
+          emoji={getRandomEmoji()}
+          lastIndex={index === conversations.length - 1}
+        />
+      ))}
+
+      {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   );
 };
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loading, login } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(userName, password);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-full md:min-w-96 mx-auto overflow-hidden">
       <div className="w-full p-4 md:p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
         <h1 className="text-2xl md:text-3xl font-semibold text-center text-gray-300">
           Login <span className="text-blue-500">Vibrant ChatApp</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Username</span>
@@ -16,6 +28,9 @@ const Login = () => {
               type="text"
               placeholder="Enter username"
               className="w-full input input-bordered h-10"
+              autoComplete="username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
             />
           </div>
           <div>
@@ -26,17 +41,26 @@ const Login = () => {
               type="password"
               placeholder="Enter password"
               className="w-full input input-bordered h-10"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <a
-            href="#"
+          <Link
+            to="/signup"
             className="text-sm hover:underline  hover:text-blue-600 mt-2 inline-block"
           >
             {"Don't"} have an account?
-          </a>
+          </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2">Login</button>
+            <button className="btn btn-block btn-sm mt-2" disabled={loading}>
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
+            </button>
           </div>
         </form>
       </div>
